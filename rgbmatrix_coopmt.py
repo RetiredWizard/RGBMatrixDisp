3f6a8df82cff21845ae4aab4058a0786320ae134
RetiredWizard <github@retiredwizard.com>
2025-01-13 00:59:06 -0500

    start documenting library

--- rgbmatrix_coopmt.py
+++ rgbmatrix_coopmt.py
@@ -1,3 +1,11 @@
+"""
+`rgbmatrix` - Hub75 RGB Matrix driver
+====================================================
+
+* Author(s): RetiredWizard
+
+"""
+
 from sys import stdin,implementation
 import select
 try:
@@ -12,10 +20,108 @@ try:
 except:
     import time as adafruit_ticks
 
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/retiredwizard/RGBMatrixDisp.git"
 
 class RGBMatrix:
+    """
+    A driver for HUB75 RGB matrix display panels.
+
+    The RGBMatrix.refresh() method must be called as frequently as possible to maintain the
+    displayed image. To help faciliate the continual refresh of the matrix this library provides
+    an RGBMatrix.sleep() method which will refresh the display while sleeping and an
+    RGBMatrix.input() method which will refresh the display while waiting for user input.
+
+    :param int rows: The number of rows in the RGB matrix.
+    :param int cols: The number of columns in the RGB matrix.
+    :param float brightness: A brightness modification for tweaking the uneven brightness resulting
+        from differing row timings due to speed optimizations. (a value of 0 disables the feature)
+    :param list[str] addrPins: String representations of the matrix's address pins in the order
+        (A, B, C, D, ...). For CircuitPython the strings should be BOARD attributes and for 
+        MicroPython the strings should be valid machine.Pin parameters.
+    :param list[str] rgbPins: String representations of the matrix's address pins in the order
+        (R1, G1, B1, R2, G2, B2). For CircuitPython the strings should be BOARD attributes and for 
+        MicroPython the strings should be valid machine.Pin parameters.
+    :param str clockPin: String representations of the matrix's clock pin. For CircuitPython the
+        string should be a BOARD attribute and for MicroPython the string should be valid machine.Pin parameter.
+    :param str latchPin: String representations of the matrix's latch pin. For CircuitPython the
+        string should be a BOARD attribute and for MicroPython the string should be valid machine.Pin parameter.
+    :param str OEPin: String representations of the matrix's output enable pin. For CircuitPython the
+        string should be a BOARD attribute and for MicroPython the string should be valid machine.Pin parameter.
+    :param list[str] unused_rgbPins: String representations of the matrix's unused address pins in the order
+        (R1, G1, B1, R2, G2, B2). Select colors may be excluded from processing in order to improve
+        update speed and reduce flickering. Any pins linsted in the parameter should be omitted from the 
+        rgbPins parameter. For CircuitPython the strings should be BOARD attributes and for MicroPython the
+        strings should be valid machine.Pin parameters.
+    
+    .. py:method:: RGBMatrix.deinit()
+
+        Attempts to free up used memory and release locked resources (CircuitPython Pins)
+
+    .. py:method:: RGBMatrix.serial_bytes_available(timeout=1)
+
+        Performs a non-blocking check to see if any uart input is available for processing
+
+    .. py:method:: RGBMatrix.fill(color)
+
+        Colors all pixels the given ***color***.
+
+    .. py:method:: RGBMatrix.input(prompt=None,silent=False)
+
+        Displays a prompt if provided and waits for user input. While waiting the RGB matrix display is
+        refreshed. If the slient parameter is set to True, the users input is not echo/displayed.
+
+    .. py:method:: RGBMatrix.sleep(seconds,slow=False)
+
+        sleeps for a given number of seconds. While sleeping the RGB matrix display is refreshed.
+        If the slow parameter is set to True, display speed optimizations will be turned off resulting
+        in more flicker but a more uniform display brigtness across rows.
+
+    .. py:method:: RGBMatrix.off()
+
+        Turns the display off.
+
+    .. py:method:: RGBMatrix.refresh()
+
+        Refreshes the RGB matrix display. This function must be performed a frequently as possible.
+
+    .. py:method:: RGBMatrix.sendrow(row)
+
+        Refreshes a single row of the display.
+
+    .. py:method:: RGBMatrix.value(row,col)
+
+        Returns the color value currently being display at the (row,col) point.
+
+    .. py:method:: RGBMatrix.point(row,col,color)
+
+        Sets the color value to be displayed at the (row,col) point.
+
+    .. py:method:: RGBMatrix.line(row0, col0, row1, col1, color=1)
+
+        Draws a straight line of color between points (row0,col0) and (row1,col1). There is likely no 
+        performance advanage of using this method over the adafruit_gfx.gfx line method. To use the
+        adafruit_gfx library with Micropython the Python source version should be downloaded from 
+        github (https://github.com/adafruit/Adafruit_CircuitPython_GFX)
+
+    .. py:method:: RGBMatrix.circle(centrow,centcol,radius,color=1)
+
+        Draws a circle of radius and color centered at points (centrow,centcol). There is likely no 
+        performance advanage of using this method over the adafruit_gfx.gfx circle method. To use the
+        adafruit_gfx library with Micropython the Python source version should be downloaded from 
+        github (https://github.com/adafruit/Adafruit_CircuitPython_GFX)
+
+    .. py:method:: RGBMatrix.dump()
+
+        Prints a matrix to the serial terminal representing the RGB matrix framebuffer.
+
+    """
 
     def __init__(self,rows,cols,brightness,addrPins,rgbPins,clockPin,latchPin,OEPin,unused_rgbPins=None):
+
+        if rows != 2 ** (len(addrPins)+1):
+            raise ValueError(f'A matrix with {rows} rows requires {len(bin(rows))-4} Address Pins')
+
         self.rows = rows
         self.cols = cols
         self.brightness = brightness
